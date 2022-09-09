2b9ca04ba26cd084b8d391891c897d7fd6e4a11f
Amanda Luiza <amandaluiza3225@gmail.com>
2022-09-09 16:47:42 -0300

    api de carros

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Navbar />
+     <h1>Olá, este site é de carros</h1>
+      <h2>Venha conferir</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+function Navbar() {
+
+
+  return (
+    <div id="Navbar">
+        <img id='icon' src="carro.png" alt="carro" />
+        <ul id='list'>
+            <li>ferrari</li> 
+            <li>celta</li>    
+            <li>toyota</li> 
+        </ul>
+
+    
+    </div>
+  )
+}
+
+export default Navbar
